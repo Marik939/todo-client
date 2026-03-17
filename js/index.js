6433bf3a3057b344e43f39a806d4f7c88da16555
marik <mmandryk25@students.oamk.fi>
2026-03-17 18:04:59 +0200

    Initial frontend for Render

---
+++ js/index.js
@@ -0,0 +1,48 @@
+const BACKEND_ROOT_URL = 'http://127.0.0.1:3001'
+import { Todos } from "./class/Todos.js"
+
+const todos = new Todos(BACKEND_ROOT_URL)
+
+const list = document.querySelector('ul')
+const input = document.querySelector('input')
+
+// Рендер одной задачи
+const renderTask = (task) => {
+  const li = document.createElement('li')
+  li.setAttribute('class', 'list-group-item')
+  li.innerHTML = task.getText()
+  list.append(li)
+}
+
+// Загрузка всех задач
+const getTasks = () => {
+  input.disabled = true
+  todos.getTasks()
+    .then((tasks) => {
+      tasks.forEach(task => renderTask(task))
+      input.disabled = false
+    })
+    .catch((error) => {
+      alert(error)
+    })
+}
+
+// Добавление новой задачи
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault()
+    const taskText = input.value.trim()
+    if (taskText !== '') {
+      todos.addTask(taskText)
+        .then((task) => {
+          renderTask(task)
+          input.value = ''
+          input.focus()
+        })
+        .catch((error) => alert(error))
+    }
+  }
+})
+
+// Запуск при загрузке страницы
+getTasks()
